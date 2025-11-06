035590074a65b09892d90809567c56b2d8f78c43
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2025-11-06 16:45:19 -0300

    ci: adiciona arquivos kubernetes

--- src/test/java/br/com/ferraz/blog/BlogApplicationTests.java
+++ src/test/java/br/com/ferraz/blog/BlogApplicationTests.java
@@ -1,9 +1,13 @@
 package br.com.ferraz.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogApplicationTests {
 
 	@Test
